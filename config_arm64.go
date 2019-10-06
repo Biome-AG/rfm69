a7c08f579801709169d0bded11c6dbfe718cd238
Chris Blake <chrisrblake93@gmail.com>
2019-10-06 09:36:35 -0500

    Update ARM64 Config

--- config_arm64.go
+++ config_arm64.go
@@ -2,12 +2,11 @@
 
 package rfm69
 
-// Configuration for Raspberry Pi Zero W with Adafruit RFM69HCW bonnet:
-// https://www.adafruit.com/product/4072
+// Configuration for NanoPi Neo2 LTS
 
 const (
 	spiDevice    = "/dev/spidev0.0"
 	spiSpeed     = 6000000 // Hz
-	interruptPin = 0      // GPIO for receive interrupts
-	resetPin     = 0      // GPIO for hardware reset
+	interruptPin = 203     // GPIO for receive interrupts
+	resetPin     = 6      // GPIO for hardware reset
 )
